9c093047c8613c7ab04f9403d392b9610372bfa6
Leora <143778430+Leorasaharia@users.noreply.github.com>
2024-09-27 01:12:06 +0530

    Time: 0 ms (100%), Space: 7.8 MB (98.22%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i=s.size()-1;
+        int l=0;
+        while(s[i]==' '){
+            i--;
+        }
+        while(i>=0 && s[i]!=' '){
+            l++;
+            i--;
+        }
+        return l;
+    }
+};
