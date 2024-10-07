63eb3448213cacebb4880ed565b22b90fe39c1e6
Leora <143778430+Leorasaharia@users.noreply.github.com>
2024-10-07 12:59:05 +0530

    Time: 16 ms (15.99%), Space: 8.5 MB (76.56%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int isPalindrome(int x){
+        if(x<0||(x%10==0 && x!=0)) return 0;
+        int rev=0,a=x;
+        while(a>rev){
+            rev=rev*10 +a%10;
+            a/=10;
+        }
+        return a==rev||a==rev/10;
+    }
+};
