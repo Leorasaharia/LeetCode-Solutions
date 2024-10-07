b1b6f53bfababafcb276320bfb00f408ba5fb324
Leora <143778430+Leorasaharia@users.noreply.github.com>
2024-10-07 13:21:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string res="";
+        int c=0;
+        int i=a.size()-1;
+        int j=b.size()-1;
+        while(i>=0||j>=0||c){
+            int sum=c;
+            if(i>=0){
+                sum+=a[i--]-'0';
+                }
+            if(j>=0){
+                sum+=b[j--]-'0';
+            }
+            res=char(sum%2+'0');
+            res++;
+            c=sum/2;
+        }
+        return res;
+    }
+};
