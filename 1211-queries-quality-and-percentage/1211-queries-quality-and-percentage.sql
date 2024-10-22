c864188e9c2e86499d3f5f85aa8287a0979c8e6d
Leora <143778430+Leorasaharia@users.noreply.github.com>
2024-10-22 17:07:32 +0530

    Time: 362 ms (44.52%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,1 +1,4 @@
-select query_name, round(avg(rating * 1.0 / position), 2) as quality, round(100.0 * sum(case when rating < 3 then 1 else 0 end) / count(*), 2) as poor_query_percentage from queries group by query_name;
+select query_name, round(avg(rating * 1.0 / position), 2) as quality, round(100.0 * sum(case when rating < 3 then 1 else 0 end) / count(*), 2) as poor_query_percentage 
+from queries 
+where query_name is not null 
+group by query_name;
