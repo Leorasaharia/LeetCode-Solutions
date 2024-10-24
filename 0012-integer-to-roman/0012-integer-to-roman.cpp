a7ba3c6f8b9cca241676e928f09da130f404d7b8
Leora <143778430+Leorasaharia@users.noreply.github.com>
2024-10-24 20:53:36 +0530

    Time: 0 ms (100%), Space: 8.2 MB (92.65%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
-    string intToRoman(int n){
+    string intToRoman(int n) {
         string r="";
-        int v[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        string s[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        for(int i=0;i<13;++i){
-            while(n>=v[i]){
-                r+=s[i];
-                n-=v[i];
-            }
+        while(n>0){
+            if(n>=1000){r+='M';n-=1000;}
+            else if(n>=900){r+="CM";n-=900;}
+            else if(n>=500){r+='D';n-=500;}
+            else if(n>=400){r+="CD";n-=400;}
+            else if(n>=100){r+='C';n-=100;}
+            else if(n>=90){r+="XC";n-=90;}
+            else if(n>=50){r+='L';n-=50;}
+            else if(n>=40){r+="XL";n-=40;}
+            else if(n>=10){r+='X';n-=10;}
+            else if(n>=9){r+="IX";n-=9;}
+            else if(n>=5){r+='V';n-=5;}
+            else if(n>=4){r+="IV";n-=4;}
+            else if(n>=1){r+='I';n-=1;}
         }
         return r;
     }
