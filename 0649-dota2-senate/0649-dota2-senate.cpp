9d6df673587b14e6ec3b6d449fba12e124bdb057
Leora <143778430+Leorasaharia@users.noreply.github.com>
2024-10-31 10:39:19 +0530

    Time: 0 ms (100%), Space: 10.7 MB (47.35%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    string predictPartyVictory(string s) {
-        queue<int> r, d;
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            if (s[i]=='R'){
-                r.push(i);
+    string predictPartyVictory(string senate) {
+        int s=senate.size();
+        queue<int> rad,dir;
+        for(int i=0;i<s;i++){
+            if(senate[i]=='R'){
+                rad.push(i);
+            }else{
+                dir.push(i);
             }
-            else{
-                d.push(i);
+        }if(rad.size()==0){
+            return "Dire";
+        }else if(dir.size()==0){
+            return "Radiant";
+        }while(!rad.empty()&& !dir.empty()){
+            if(rad.front()<dir.front()){
+                dir.pop();
+                int r=rad.front();
+                rad.push(r+s);
+                rad.pop();
+            }else{
+                rad.pop();
+                int dr=dir.front();
+                dir.pop();
+                dir.push(dr+s);
             }
         }
-        while(!r.empty()&&!d.empty()){
-            int x=r.front(),y=d.front();
-            r.pop();
-            d.pop();
-            if(x<y){
-                r.push(x + n);
-            }
-            else{
-                d.push(y + n);
-            }
-        }
-        return r.empty()?"Dire":"Radiant";
+        return rad.size()==0?"Dire":"Radiant";
     }
 };
