23d4ebeaf185eed18da532e594611e32df719c86
Leora <143778430+Leorasaharia@users.noreply.github.com>
2024-11-28 16:14:34 +0530

    Time: 0 ms (100%), Space: 12.3 MB (5.6%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hIndex(vector<int>& c){
+        int n=c.size();
+        sort(c.begin(),c.end());
+        int h=0;
+        for(int i=0;i<n;i++){
+            if(c[i]>=n-i){
+                h=n-i;
+                break;
+            }
+        }
+        return h;
+    }
+};
