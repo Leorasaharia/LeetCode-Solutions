63fb27a0696a1b63d32bbda82a32ab3fda18bfc6
Leora <143778430+Leorasaharia@users.noreply.github.com>
2024-12-21 17:50:00 +0530

    Time: 0 ms (100%), Space: 18.5 MB (22.36%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSymmetric(TreeNode* r){
+        return isSymm(r,r);
+    } 
+private:
+    bool isSymm(TreeNode* l,TreeNode* r){
+        if(!l &&!r){
+            return true;
+        }
+        if(l&&r&&l->val==r->val){
+            bool left_sym = isSymm(l->left,r->right);
+            bool right_sym = isSymm(l->right,r->left);
+            return left_sym && right_sym;
+        }
+        return false;
+    }
+};
