781cbc9dd20e27302980caacd6bae371b2937124
Leora <143778430+Leorasaharia@users.noreply.github.com>
2024-12-28 15:58:02 +0530

    Time: 8 ms (9.17%), Space: 19.5 MB (42.43%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    vector<TreeNode*>gT(int n,int s=1){
+    vector<TreeNode*>generateTrees(int n,int s=1){
         vector<TreeNode*>a;
         if(n<s){
             return{nullptr};
         }
         for(int i=s;i<=n;i++){
-            for(auto l:gT(i-1,s)){
-                for(auto r:gT(n,i+1)) 
+            for(auto l:generateTrees(i-1,s)){
+                for(auto r:generateTrees(n,i+1)) 
                     a.push_back(new TreeNode(i,l,r));
             }
         }
