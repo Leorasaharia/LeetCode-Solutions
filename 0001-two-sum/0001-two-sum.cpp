248a900863b375d9d13c9a1c9804f309fd347de4
Leora <143778430+Leorasaharia@users.noreply.github.com>
2024-12-30 13:39:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& n, int t){
+        for(int i=0;i<n.size();i++){
+            for(int j=i+1;j<n.size();j++){
+                if(n[i]+n[j]==t){
+                    return{i,j};
+                }
+            }
+        }
+        return ;
+    }
+};
