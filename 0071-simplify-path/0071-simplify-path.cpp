0bc615b271502a554a58a2fe04355c78b54c199f
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-01-12 12:05:20 +0530

    Time: 4 ms (38.12%), Space: 10.4 MB (81.75%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -3,21 +3,20 @@ public:
     string simplifyPath(string path) {
         vector<string>s;
         string res,temp;
-        for(int i=0;i<path.size();++i){
-            if(path[i]=='/'){
-                continue;
+        int i=0;
+        do{
+            while(i<path.size()&&path[i]=='/'){
+                i++;
             }
+            temp="";
             while(i<path.size()&&path[i]!='/'){
                 temp+=path[i++];
             }
             if(temp==".."){
                 if(!s.empty())s.pop_back();
-                }
-            else if
-                (temp!=".")s.push_back(temp);
-            
-            temp.clear();
-        }
+            }
+            else if(temp!=""&&temp!=".")s.push_back(temp);
+        }while(i<path.size());
         for(auto&d:s){
             res+="/"+d;
         }
