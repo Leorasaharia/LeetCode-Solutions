cb859aae9d7136dffb5d6c400827e9abc0ad19f9
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-02-02 00:31:36 +0530

    Time: 0 ms (100%), Space: 26.1 MB (62.66%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     int trap(vector<int>&h){
-        int l=0,r=h.size()-1;
-        int lmax=0,rmax=0;
-        int res=0;
+        int l=0;
+        long long r=h.size()-1;
+        int lm=0,rm=0,res=0;
         while(l<r){
             if(h[l]<h[r]){
-                h[l]>=lmax?lmax=h[l]:res+=lmax-h[l];
+                if(h[l]>=lm){
+                    lm=h[l];
+                }
+                else{
+                    res+=lm-h[l];
+                } 
                 l++;
             }else{
-                h[r]>=rmax?rmax=h[r]:res+=rmax-h[r];
+                if(h[r]>=rm){
+                    rm=h[r];
+                }
+                else{
+                    res+=rm-h[r];
+                }
                 r--;
             }
         }
