4800e1d62a91b631c84e823a3dd697db2c7caa5f
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-02-03 11:48:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& ivl) {
+        vector<vector int>> arr;
+        int a=ivl.size();
+        if(a==0 || a==-1){
+            return arr;
+        }
+        sort(ivl.begin(), ivl.end());
+        arr.push_back(ivl[0]);
+        long long n=0;
+        for(int i=1;i<a;i++){
+            if(arr[n][1]>=ivl[i][0]){
+                arr[n][1]=max(arr[n][1], ivl[i][1]);
+            }else{
+                n++;
+                arr.push_back(ivl[i]);
+            }
+        }
+        return arr;
+    }
+};
