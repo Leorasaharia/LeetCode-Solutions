ce7013c1b54d99817d07e971cf28a230e3aa839f
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-02-11 12:02:01 +0530

    Time: 0 ms (100%), Space: 9.9 MB (31.48%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     bool isInterleave(string&s1,string&s2,string&s3){
-        if(s1.size()+s2.size()!=s3.size()){
-            return false; 
+        long long m=s1.size();
+        long long n=s2.size();
+        if(m+n!=s3.size()){
+            return false;
         }
-        return helper(s1,s2,s3,0,0,0);
+        vector<vector<int>> temp(m+1,vector<int>(n+1,-1));
+        return helper(s1,s2,s3,0,0,temp);
     }
-    bool helper(string&s1,string&s2,string&s3,int i,int j,int k){
-        if(k==s3.size()){
-            return i==s1.size()&&j==s2.size();
+    bool helper(string&s1,string&s2,string&s3,int i,int j,vector<vector<int>>&temp){
+        if(i==s1.size()&&j==s2.size()){
+            return true;
         }
-        bool S1=false, S2=false;
-        if(i<s1.size()&&s1[i]==s3[k]){
-            S1=helper(s1,s2,s3,i+1,j,k+1);
+        if(temp[i][j]!=-1){
+            return temp[i][j];
         }
-        if(j<s2.size()&&s2[j]==s3[k]){
-            S2=helper(s1,s2,s3,i,j+1,k+1);
+        bool S1=false,S2=false;
+        if(i<s1.size()&&s1[i]==s3[i+j]){ 
+            S1=helper(s1,s2,s3,i+1,j,temp);
         }
-        return S1||S2;
+        if(j<s2.size()&&s2[j]==s3[i+j]){
+            S2=helper(s1,s2,s3,i,j+1,temp);
+        }
+        return temp[i][j]=S1||S2;
     }
 };
