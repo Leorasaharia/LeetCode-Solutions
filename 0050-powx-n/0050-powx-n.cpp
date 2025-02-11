39001d7552dec9af3cca7e04c248529d27008400
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-02-11 12:06:49 +0530

    Time: 0 ms (100%), Space: 8.9 MB (9.04%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
-    double myPow(double x,int n){
-        long long num=n;
-        if(num<0){
-            x=1/x;
-            num=-num;
-        }
-        double res=1;
-        while(num>0){
-            if(num&1){
-                res*= x;
-            }
-            x*=x;
-            num>>=1;
-        }
-        return res;
+    double myPow(double x, int n) {
+        return(pow(x,n));
     }
 };
