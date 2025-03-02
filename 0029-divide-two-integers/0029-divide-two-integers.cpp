e272cfba22453dfe8dc438a3ae1aca572fe3a6d6
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-03-02 13:05:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int divide(int a, int b) {
+        if(a==INT_MIN && b==-1){
+            return INT_MAX; 
+        }  
+        bool neg=(a<0) ^(b<0);
+        long x=abs((long)a);
+        long y=abs((long)b);
+        int res=0;
+        vector<long> dp,m;
+        long var=y;
+        int mul=1;
+        while(var<= x){
+            dp.push_back(var);
+            m.push_back(mul);
+            var<<=1;
+            mul<<=1;
+        }
+        for(int i=dp.size()-1;i>=0;--i){
+            if(x>=dp[i]){
+                x-=dp[i];
+                res+=m[i];
+            }
+        }
+        return neg?-res:res;
+    }
+};
