181ca8c6dec344825f1f9eedafac191820c411fc
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-03-02 13:49:16 +0530

    Time: 23 ms (68.63%), Space: 17.8 MB (80.54%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amt) {
-        sort(coins.begin(), coins.end(), greater<int>());
-        int count = 0;
-
-        for (int c : coins) {
-            if (amt >= c) {
-                count += amt / c;
-                amt %= c;
+        sort(coins.begin(),coins.end(),greater<int>());
+        vector<int> dp(amt+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=amt;++i){
+            for(int c:coins){
+                if(i>=c && dp[i-c]!=INT_MAX){
+                    dp[i]=min(dp[i],dp[i-c]+1);
+                }
             }
         }
-
-        return amt == 0 ? count : -1;
+        return dp[amt]==INT_MAX?-1:dp[amt];
     }
 };
+
+//bottom up 
