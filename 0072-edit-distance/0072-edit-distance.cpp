259db8eb8147ea3eede778c0ebf67e53b3866fde
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-03-03 11:09:46 +0530

    Time: 0 ms (100%), Space: 10 MB (99.79%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
+    static int c(char a, char b) {
+        return a != b;
+    }
     int minDistance(string w1, string w2) {
-        int m = w1.size();
-        int n = w2.size();
+        int m = w1.size(), n = w2.size();
         vector<int> dp(n + 1);
-        for (int j = 0; j <= n; j++) {
+        for (int j = 0; j <= n; j++){
             dp[j] = j;
         }
         for (int i = 1; i <= m; i++) {
@@ -12,10 +14,7 @@ public:
             dp[0] = i;
             for (int j = 1; j <= n; j++) {
                 int temp = dp[j];
-                if (w1[i - 1] == w2[j - 1]) 
-                    dp[j] = prev;
-                else 
-                    dp[j] = 1 + min({dp[j - 1], dp[j], prev});
+                dp[j] = min({dp[j - 1] + 1, dp[j] + 1, prev + c(w1[i - 1], w2[j - 1])});
                 prev = temp;
             }
         }
