77b412a682e171294f041d137771671ca827ad20
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-03-09 20:27:36 +0530

    Time: 66 ms (76.15%), Space: 108.1 MB (32.85%) - LeetHub

--- 3208-alternating-groups-ii/3208-alternating-groups-ii.cpp
+++ 3208-alternating-groups-ii/3208-alternating-groups-ii.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& c, int k) {
         int n=c.size();
-        int cnt=0,tmp=1;
-        for(int i=1; i<k; i++){
-            if(c[i]==c[i-1]){
-                tmp=0;
-            }
+        for(int i=0;i<k;i++){
+            c.push_back(c[i]);
         }
-        cnt+=tmp;
-        for(int i=1; i<n; i++){
-            if(c[(i+k-2)%n]==c[(i+k-1)%n]){
-                tmp=0;
+        int tmp=-1;
+        int cnt=0;
+        for(int i=1;i<n+k-1;i++){
+            if(c[i]==c[i-1]){
+                tmp=i-1;
+                continue;
             }
-            if(c[i-1]==c[i]){
-                tmp=1;
+            if(i-tmp>=k){
+                cnt++;
             }
-            cnt+=tmp;
         }
         return cnt;
     }
