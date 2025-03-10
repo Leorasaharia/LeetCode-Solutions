9422128452bf767036b7a428e99ae658d690809f
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-03-10 14:39:36 +0530

    Time: 274 ms (32.6%), Space: 44 MB (60%) - LeetHub

--- 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    long long countOfSubstrings(string w, int k) {
+        int n = w.length();
+        int i = 0, j = 0;
+        long long ans = 0;
+        map<char, int> mp;
+        int cnt = 0;
+
+        while (j < n) {
+            if (w[j] == 'a' || w[j] == 'e' || w[j] == 'i' || w[j] == 'o' || w[j] == 'u') {
+                mp[w[j]]++;
+            } else {
+                cnt++;
+            }
+
+            while (mp.size() == 5 && cnt >= k) {
+                ans += w.size() - j;
+
+                if (mp.find(w[i]) != mp.end()) {
+                    mp[w[i]]--;
+                    if (mp[w[i]] == 0) {
+                        mp.erase(w[i]);
+                    }
+                } else {
+                    cnt--;
+                }
+                i++;
+            }
+            j++;
+        }
+        return ans - Solve(w, k + 1);
+    }
+
     long long Solve(string& w, int k) {
         int n = w.length();
         int i = 0, j = 0;
@@ -30,9 +62,5 @@ public:
             j++;
         }
         return ans;
-    }
-
-    long long countOfSubstrings(string w, int k) {
-        return Solve(w, k) - Solve(w, k + 1);
     }
 };
