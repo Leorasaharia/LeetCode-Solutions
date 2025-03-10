186a76432952a67fa8a769c403803a5c95b79517
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-03-10 20:21:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,24 +1,8 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l=0,r=nums.size()-1;
-        while(l<=r){
-            int m=l+(r-l)/2;
-            if(nums[m]==target){
-                return m;}
-            if(nums[l]<=nums[m]){ 
-                if(nums[l]<=target && target<nums[m]){
-                    r=m-1; 
-                }else{
-                    l=m+1; }
-            }else{
-                if (nums[m]<target && target<=nums[r]){
-                    l=m+1;
-                }else{
-                    r=m-1; 
-                }
-            }
-        }
-        return -1; 
+    int searchInsert(vector<int>&nums,int target) {
+        int i=0;
+        while(i<nums.size()&&nums[i]<target)i++;
+        return i;
     }
 };
