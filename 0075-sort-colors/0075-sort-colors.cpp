5f6fff9c0211ddef89fc8ea2eea7af83d803e68d
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-03-12 12:02:10 +0530

    Time: 0 ms (100%), Space: 11.8 MB (6.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,16 +1,42 @@
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int l=0,m=0,h=nums.size()-1;
+//         while(m<=h){
+//             if(nums[m]==0){
+//                 swap(nums[l++],nums[m++]);
+//             }
+//             else if(nums[m]==1){
+//                 m++;
+//             }
+//             else{
+//                 swap(nums[m],nums[h--]);
+//             }
+//         }
+//     }
+// };
+
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        int l=0,m=0,h=nums.size()-1;
+    void sortColors(vector<int>&a) {
+        int h=a.size()-1;
+        int  l=0,m=0;
         while(m<=h){
-            if(nums[m]>=0 || nums[m]==0){
-                swap(nums[l++],nums[m++]);
+            if(a[m]==0){
+                int tmp=a[l];
+                a[l]=a[m];
+                a[m]=tmp;
+                l++;
+                m++;
             }
-            else if(nums[m]==1){
+            else if(a[m]==1){
                 m++;
             }
             else{
-                swap(nums[m],nums[h--]);
+                int tmp=a[m];
+                a[m]=a[h];
+                a[h]=tmp;
+                h--;
             }
         }
     }
