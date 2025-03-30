17353743bd0d995e79b467d0648a0e1c992e34a1
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-03-30 21:25:11 +0530

    Time: 40 ms (5.59%), Space: 13.5 MB (38.39%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        vector<int> freq_arr(32,0);
-        for(int e:nums){
-            for(int i=0;i<32;i++){
-                if(e&(1<<i)){
-                    freq_arr[i]++;
+    int singleNumber(vector<int>& nums){
+        int n=nums.size();
+        for(int i=0;i<n;++i){
+            bool found=false;
+            for(int j=0;j<n;++j){
+                if(i!=j && nums[i]==nums[j]){
+                    found=true;
+                    break;
                 }
             }
-        }
-        int result=0;
-        for(int i=0;i<32;i++){
-            if(freq_arr[i]%3==1){
-                result+=(1<<i);
+            if(!found){
+                return nums[i];
             }
         }
-        return result;
+        return -1;
     }
 };
