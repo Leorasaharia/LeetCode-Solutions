6ec899cdff6e0bd28594494c49331e55793f7ec4
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-04-12 12:25:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& n, int k) {
+    int n=a.size();
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<=i+k&&j<n;j++){
+            if(a[i]==a[j])return true;
+            return false;
+        }
+    }
+    }
+};
