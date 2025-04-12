7112fc9561946bc8ca608d36da4cf56cabf8f1f5
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-04-12 12:26:00 +0530

    Time: 1565 ms (7.68%), Space: 74.1 MB (91.22%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    bool containsNearbyDuplicate(vector<int>& n, int k) {
-    int n=a.size();
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<=i+k&&j<n;j++){
-            if(a[i]==a[j])return true;
-            return false;
-        }
-    }
+    bool containsNearbyDuplicate(vector<int>& a, int k) {
+        int n=a.size();
+        for(int i=0;i<n;i++)
+            for(int j=i+1;j<=i+k&&j<n;j++)
+                if(a[i]==a[j])return true;
+        return false;
     }
 };
