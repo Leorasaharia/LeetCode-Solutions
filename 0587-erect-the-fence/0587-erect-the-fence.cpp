932ea88f4ac198be1ca602f8f0c23742a72e50bb
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-05-11 11:17:51 +0530

    Time: 7 ms (80.97%), Space: 24.3 MB (81.31%) - LeetHub

--- 0587-erect-the-fence/0587-erect-the-fence.cpp
+++ 0587-erect-the-fence/0587-erect-the-fence.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
-int findeqn(pair<int,int>&p1, pair<int,int>&p2, pair<int,int>p3)
-{
-    int x1=p1.first;
-    int x2=p2.first;
-    int x3=p3.first;
-    int y1=p1.second;
-    int y2=p2.second;
-    int y3=p3.second;
-    return (y3-y2)*(x2-x1)-(y2-y1)*(x3-x2);
-}
-    vector<vector<int>> outerTrees(vector<vector<int>>& trees) {
-       sort(begin(trees), end(trees));
-       vector<vector<int>> result;
-       deque<pair<int,int>>upper,lower;
-       for(auto&point:trees){
-        int l=lower.size();
-        int u=upper.size();
-        while(l>=2 && findeqn(lower[l-2],lower[l-1], {point[0],point[1]})<0){
-            l--;
-            lower.pop_back();
+    long long findeqn(pair<int,int>&p1, pair<int,int>&p2, pair<int,int>p3) {
+        long long x1=p1.first,x2=p2.first,x3=p3.first;
+        long long y1=p1.second,y2=p2.second,y3=p3.second;
+        return (y3-y2)*(x2-x1)-(y2-y1)*(x3-x2);
+    }
+
+    vector<vector<int>> outerTrees(vector<vector<int>>& t) {
+        sort(begin(t), end(t));
+        deque<pair<int,int>> u,l;
+        for(auto& p:t){
+            while(l.size()>=2 && findeqn(l[l.size()-2],l[l.size()-1],{p[0],p[1]})<0){
+                l.pop_back();
+            }
+            while(u.size()>=2 && findeqn(u[u.size()-2],u[u.size()-1],{p[0],p[1]})>0){
+                u.pop_back();
+            }
+            l.push_back({p[0],p[1]});
+            u.push_back({p[0],p[1]});
         }
-        while(u>=2 && findeqn(upper[l-2],upper[l-1], {point[0],point[1]})>0){
-            u--;
-            upper.pop_back();
+        set<pair<int,int>> s;
+        for(auto& p:l){
+            s.insert(p);
         }
-        upper.push_back({point[0],point[1]});
-        lower.push_back({point[0],point[1]});
-       }
-       set<pair<int,int>> s;
-       for(auto&point:upper){
-        s.insert(point);
-       }
-       for(auto&point:lower){
-        s.insert(point);
-       }
-       for(auto&point:s){
-        result.push_back({point.first,point.second});
-       }
-       return result;
+        for(auto& p:u){
+            s.insert(p);
+        }
+        vector<vector<int>> r;
+        for(auto& p:s){
+            r.push_back({p.first,p.second});
+        }
+        return r;
     }
 };
