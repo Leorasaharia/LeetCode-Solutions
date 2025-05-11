7fc5a831d9ecb61447183b68ab529f83dccd2091
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-05-11 10:45:51 +0530

    Time: 0 ms (100%), Space: 71.6 MB (81.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        if(nums.size()==0)
-           return 0;
-           int sm_max=nums[0];
-           int sum_curr=nums[0];
-           for (int i=1;i<nums.size();++i){
-            int n=nums[i];
-            sum_curr=max(sum_curr+n,n);
-            sm_max=max(sm_max,sum_curr);
-           }
-        return sm_max;
+        int n=nums.size();
+        int mx=nums[0];
+        int curr=nums[0];
+        for(int i=1;i<n;i++){
+            curr=max(curr+nums[i],nums[i]);
+            mx=max(mx,curr);
+        }
+        return mx;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+//         if(nums.size()==0)
+//            return 0;
+//            int sm_max=nums[0];
+//            int sum_curr=nums[0];
+//            for (int i=1;i<nums.size();++i){
+//             int n=nums[i];
+//             sum_curr=max(sum_curr+n,n);
+//             sm_max=max(sm_max,sum_curr);
+//            }
+//         return sm_max;
+//     }
+// };
