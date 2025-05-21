0e8a2ac390554a34e86ba7a1bf08ee55a0f4f645
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-05-21 12:36:53 +0530

    Time: 7 ms (43.54%), Space: 23.7 MB (80.31%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& ivl) {
-        vector<vector<int>> arr;
-        int a=ivl.size();
-        if(a==0 || a==-1){
-            return arr;
+    vector<vector<int>> merge(vector<vector<int>>& a) {
+        sort(a.begin(), a.end());
+        vector<vector<int>> r;
+        for (auto& x : a) {
+            if (r.empty() || r.back()[1] < x[0])
+                r.push_back(x);
+            else
+                r.back()[1] = max(r.back()[1], x[1]);
         }
-        sort(ivl.begin(), ivl.end());
-        arr.push_back(ivl[0]);
-        long long n=0;
-        for(int i=1;i<a;i++){
-            if(arr[n][1]>=ivl[i][0]){
-                arr[n][1]=max(arr[n][1], ivl[i][1]);
-            }else{
-                n++;
-                arr.push_back(ivl[i]);
-            }
-        }
-        return arr;
+        return r;
     }
 };
