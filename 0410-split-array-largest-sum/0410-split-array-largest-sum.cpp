bb87e953dc08fc461d9803eed81af9734be8c29e
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-05-27 19:30:21 +0530

    Time: 0 ms (100%), Space: 10.4 MB (87.63%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    int splitArray(vector<int>&a,int k) {
-        int n=a.size();
-        if(k>n){
-            return -1;
-        }
-        int mn=INT_MAX;
-        int t=1<<(n-1);
-        for(int m=0;m<t;m++){
-            int s=0;
-            for(int i=0;i<n-1;i++){
-                if(m&(1<<i)){
-                    s++;
-                }
+    bool chk(vector<int>&a,int k,int m){
+        int s=0,c=1;
+        for(int x:a){
+            if(x>m)return 0;
+            if(s+x>m){
+                c++;
+                s=0;
             }
-            if(s==k-1){
-                int mx=0,c=0;
-                for(int i=0;i<n;i++){
-                    c+=a[i];
-                    if(i==n-1||(m&(1<<i))){
-                        if(c>mx){
-                            mx=c;
-                        }
-                        c=0;
-                    }
-                }
-                if(mx<mn){
-                    mn=mx;
-                }
-            }
+            s+=x;
         }
-        return mn;
+        return c<=k;
+    }
+    
+    int splitArray(vector<int>&a,int k){
+        int l=0,r=0;
+        for(int x:a){
+            l=max(l,x);
+            r+=x;
+        }
+        while(l<r){
+            int m=l+(r-l)/2;
+            if(chk(a,k,m))r=m;
+            else l=m+1;
+        }
+        return l;
     }
 };
