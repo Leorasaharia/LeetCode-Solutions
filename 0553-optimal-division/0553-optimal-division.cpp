903c9aacf4e8c2e47a57612c2576786564bd9952
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-06-03 11:29:17 +0530

    Time: 0 ms (100%), Space: 10.4 MB (80.46%) - LeetHub

--- 0553-optimal-division/0553-optimal-division.cpp
+++ 0553-optimal-division/0553-optimal-division.cpp
@@ -2,50 +2,42 @@ class Solution{
 public:
     string optimalDivision(vector<int>&nums){
         int n=nums.size();
-        string r;
-        int v=nums[0];
-        if(v==0)r.push_back('0');
-        else{
+        auto add=[&](int x,string& s){        //converting the integre to string 
+            if(x==0){
+                s.push_back('0');
+                return;
+            }
             string t;
-            while(v>0){
-                int d=v%10;
-                t.push_back('0'+d);
-                v/=10;
+            while(x){
+                t.push_back('0'+x%10);
+                x/=10;
             }
-            for(int i=t.size()-1;i>=0;--i)r.push_back(t[i]);
-        }
-        if(n==1)return r;
-        if(n==2){
-            r.push_back('/');
-            v=nums[1];
-            if(v==0)r.push_back('0');
-            else{
-                string t;
-                while(v>0){
-                    int d=v%10;
-                    t.push_back('0'+d);
-                    v/=10;
-                }
-                for(int i=t.size()-1;i>=0;--i)r.push_back(t[i]);
+            for(int i=t.size()-1;i>=0;--i){
+                s.push_back(t[i]);
             }
-            return r;
+        };
+        string ans;
+        if(n==1){
+            add(nums[0],ans);
+            return ans;
         }
-        r+="/(";
-        for(int idx=1;idx<n;++idx){
-            v=nums[idx];
-            if(v==0)r.push_back('0');
-            else{
-                string t;
-                while(v>0){
-                    int d=v%10;
-                    t.push_back('0'+d);
-                    v/=10;
-                }
-                for(int i=t.size()-1;i>=0;--i)r.push_back(t[i]);
+        else if(n==2){
+            add(nums[0],ans);
+            ans.push_back('/');
+            add(nums[1],ans);
+            return ans;
+        }
+        else{
+            add(nums[0],ans);
+            ans.push_back('/');
+            ans.push_back('(');
+            for(int i=1;i<n-1;++i){
+                add(nums[i],ans);
+                ans.push_back('/');
             }
-            if(idx<n-1)r.push_back('/');
+            add(nums[n-1],ans);
+            ans.push_back(')');
+            return ans;
         }
-        r.push_back(')');
-        return r;
     }
 };
