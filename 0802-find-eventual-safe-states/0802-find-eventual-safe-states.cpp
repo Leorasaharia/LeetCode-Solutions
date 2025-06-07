b6d928c543d5a4d9b384aa49f7e36d28e6c268e9
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-06-08 02:23:02 +0530

    Time: 4 ms (91.74%), Space: 51.2 MB (97.65%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph, int u, vector<bool>& vis, vector<bool>& rec, vector<bool>& unsafe) {
+    bool dfs(vector<vector<int>>&g,int u,vector<bool>&vis,vector<bool>&rec,vector<bool>&unsafe){
+        if(unsafe[u]){
+            return true;
+        }
+        if(vis[u]){
+            return false;
+        }
         vis[u]=1;
         rec[u]=1;
-        for(int i=0;i<graph[u].size();i++){
-            int v=graph[u][i];
-            if(!vis[v] && dfs(graph,v,vis,rec,unsafe)){
+        for(int i=0;i<g[u].size();i++){
+            int v=g[u][i];
+            if(rec[v]||dfs(g,v,vis,rec,unsafe)){
                 unsafe[u]=1;
-            }
-            else if(rec[v]){
-                unsafe[u]=1;
+                rec[u]=0;
+                return true;
             }
         }
         rec[u]=0;
-        return unsafe[u];
+        return false;
     }
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<bool>vis(n,0), rec(n,0), unsafe(n,0);
+    vector<int> eventualSafeNodes(vector<vector<int>>&g){
+        int n=g.size();
+        vector<bool>vis(n,0),rec(n,0),unsafe(n,0);
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                dfs(graph,i,vis,rec,unsafe);
+                dfs(g,i,vis,rec,unsafe);
             }
         }
-        vector<int>res;
+        vector<int>ans;
         for(int i=0;i<n;i++){
-            if(unsafe[i]==0){
-                res.push_back(i);
+            if(!unsafe[i]){
+                ans.push_back(i);
             }
         }
-        return res;
+        return ans;
     }
 };
