7ffa3c9173337e695085f0514c7a06595b64ef8a
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-07-02 18:55:07 +0530

    Time: 0 ms (100%), Space: 36.5 MB (85.96%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -4,17 +4,18 @@ public:
     int i;
     int find(int x){
         if(parent[x]==x){
-            return x;
+            return x; //x is its own parent
         }
-        parent[x]=find(parent[x]);
+        parent[x]=find(parent[x]); //x is not its own parent
         return parent[x];
     }
     bool dsu(int par,int child){
         int c=find(child);
-        if(c!=child){
+        if(c!=child){ //child has a psrent 
             return false;
         }
-        int p=find(par);
+        int p=find(par); //to find the root of parent
+        //now the cycle is detected
         if(p==c){
             return false;
         }
@@ -25,20 +26,20 @@ public:
     bool validateBinaryTreeNodes(int n,vector<int>& left,vector<int>& right){
         parent.resize(n);
         for(int j=0;j<n;j++){
-            parent[j]=j;
+            parent[j]=j; //parent of own component
         }
-        vector<int> indeg(n,0);
-        i=n;
+        vector<int> indeg(n,0); //to validate the incoming edges towards vertex
+        i=n; //not 
         for(int k=0;k<n;k++){
             for(int child:{left[k],right[k]}){ 
                 if(child==-1){
                     continue;
                 }
                 indeg[child]++;
-                if(indeg[child]>1){
+                if(indeg[child]>1){ //more than 1 parent, invalid bt
                     return false;
                 }
-                if(!dsu(k,child)){
+                if(!dsu(k,child)){ //if found multiple roots, invalid bt
                     return false;
                 }
             }
