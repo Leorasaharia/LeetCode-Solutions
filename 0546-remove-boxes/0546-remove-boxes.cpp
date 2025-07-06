ce9bea50b162a434512e67d21973fe8f2fbfde4f
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-07-06 16:03:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0546-remove-boxes/0546-remove-boxes.cpp
+++ 0546-remove-boxes/0546-remove-boxes.cpp
@@ -3,31 +3,27 @@ public:
     int dp[105][105][105];
 
     int solve(vector<int>&boxes,int lt,int rt,int cnt){
-        if(lt>rt){
-            return 0;
-        }
-        if(lt==rt){
-            return (cnt+1)*(cnt+1);
-        }
-        if(dp[lt][rt][cnt]!=-1){
-            return dp[lt][rt][cnt];
-        }
+        if(lt>rt)return 0;
+        if(dp[lt][rt][cnt]!=-1)return dp[lt][rt][cnt];
+
+        while(lt+1<=rt&&boxes[lt]==boxes[lt+1])lt++,cnt++;
+
         int res=(cnt+1)*(cnt+1)+solve(boxes,lt+1,rt,0);
+
         for(int i=lt+1;i<=rt;i++){
-            if(boxes[lt]==boxes[i]){
+            if(boxes[i]==boxes[lt]){
                 res=max(res,solve(boxes,i,rt,cnt+1)+solve(boxes,lt+1,i-1,0));
             }
         }
+
         return dp[lt][rt][cnt]=res;
     }
+
     int removeBoxes(vector<int>&boxes){
-        for(int i=0;i<105;i++){
-           for(int j=0;j<105;j++){
-               for(int k=0;k<105;k++){
-                  dp[i][j][k]=-1;
-               }
-           }
-        }
-        return solve(boxes,0,(int)boxes.size()-1,0);
+        for(int i=0;i<105;i++)
+        for(int j=0;j<105;j++)
+        for(int k=0;k<105;k++)
+        dp[i][j][k]=-1;
+        return solve(boxes,0,boxes.size()-1,0);
     }
 };
