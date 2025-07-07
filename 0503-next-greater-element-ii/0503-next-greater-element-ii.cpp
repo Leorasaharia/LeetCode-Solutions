46005c2f177c92eae953d877b6ef57d0404691af
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-07-07 16:06:38 +0530

    Time: 124 ms (5.09%), Space: 27.9 MB (95.61%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>&nums){
         int n=nums.size();
-        vector<int> arr(n,-1);
+        vector<int> arr(n,-1); //this is the resulting array with -1 for all elements
         for(int i=0;i<n;i++){
-            for(int j=1;j<n;j++){
+            for(int j=1;j<n;j++){ //then we traverse for each element, checking if we find the next graeter element
                 int idx=(i+j)%n;
                 if(nums[idx]>nums[i]){
                     arr[i]=nums[idx];
