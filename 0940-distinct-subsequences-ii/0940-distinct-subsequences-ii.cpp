92f80ecb6b361f0f6a3cd72e2cab53b6b6bb69aa
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-07-17 13:57:23 +0530

    Time: 0 ms (100%), Space: 8.8 MB (87.12%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -3,20 +3,20 @@ public:
     int distinctSubseqII(string s) {
         int mod=1e9+7;
         int n=s.size();
-        int total=1;
+        int tot=1;
         int last[26];
         for(int i=0;i<26;++i){
             last[i]=0;
         }
         for(int i=0;i<n;++i){
             int c=s[i]-'a';
-            int nt=(2*total)%mod;
+            int nt=(2*tot)%mod;
             if(last[c]>0){
                 nt=(nt-last[c]+mod)%mod;
             }
-            last[c]=total;
-            total=nt;
+            last[c]=tot;
+            tot=nt;
         }
-        return (total-1+mod)%mod;
+        return (tot-1+mod)%mod;
     }
 };
