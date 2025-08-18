8ed024b81b603ad9e903619efabf76d9d2235ebf
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-08-18 15:19:16 +0530

    Time: 0 ms (100%), Space: 11.4 MB (25.92%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,21 +1,19 @@
-class Solution {
+class Solution{
 public:
-    string reverseWords(string s) {
-        vector<string> ws; string w;
-        for (char c:s){
-            if (c!=' ') 
-            w+=c;
-            else if(!w.empty()){ 
-                ws.push_back(w); 
-                w.clear(); }
+    string reverseWords(string s){
+        stringstream ss(s);
+        string w;
+        vector<string> arr;
+        while(ss>>w){
+            arr.push_back(w);
         }
-        if(!w.empty()) ws.push_back(w);
-        reverse(ws.begin(),ws.end());
+        reverse(arr.begin(),arr.end());
         string res;
-        for(int i=0;i<ws.size();i++){
-            res+=ws[i];
-            if(i<ws.size()-1) 
-            res+=" ";
+        for(int i=0;i<arr.size();i++){
+            if(i!=0){
+                res+=" ";
+            }
+            res+=arr[i];
         }
         return res;
     }
