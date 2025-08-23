58bda5040fb987ea694d6bccefa34d4aaafc9af9
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-08-23 21:18:05 +0530

    Time: 0 ms (100%), Space: 12.9 MB (68.02%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,38 +1,42 @@
-int findp(int p[], int x){
-    if(p[x]==x){
-        return x;
-    }
-    return p[x]=findp(p,p[x]);
-}
-
-bool dsu(int p[], int r[], int a, int b){
-    a=findp(p,a);  
-    b=findp(p,b); 
-    if(a==b){
-        return false;
-    } 
-    if(r[a]<r[b]){
-        swap(a,b); 
-    } 
-    p[b]=a;  
-    if(r[a]==r[b]){
-        r[a]++;
-    }
-    return true;
-}
-vector<int> findRedundantConnection(vector<vector<int>>& e) {
-    int n=e.size();  
-    int p[n+1],r[n+1];  
-    for(int i=1;i<=n;i++){ 
-        p[i]=i;
-        r[i]=0;
-    }
-    for(int i=0;i<n;i++){ 
-        int a=e[i][0];
-        int b=e[i][1];
-        if(!dsu(p,r,a,b)){
-            return {a,b};  
+struct DSU {
+    vector<int> parent;
+    DSU(int n){
+        parent.resize(n+1);
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
         }
     }
-    return {};
-}
+    int findParent(int x){
+        if(parent[x]==x){
+            return x;
+        }
+        else{
+            return parent[x]=findParent(parent[x]);
+        }
+    }
+    bool unionSet(int x,int y){
+        int a=findParent(x);
+        int b=findParent(y);
+        if(a==b){
+            return false;
+        }
+        parent[a]=b;
+        return true;
+    }
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        DSU dsu(n);
+        for(int i=0;i<n;i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            if(!dsu.unionSet(u,v)){
+                return edges[i];
+            }
+        }
+        return {};
+    }
+};
