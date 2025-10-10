321f9622a8ce94b1405aaaf9ce7305de58daf03e
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-10-10 23:00:41 +0530

    Time: 136 ms (73.59%), Space: 145.5 MB (77.46%) - LeetHub

--- 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -3,18 +3,19 @@ public:
     int maximumEnergy(vector<int>& energy, int k) {
         int n=energy.size();
         int ans=0; 
-        bool ok=false;
-        for(int i=0;i<n;i++){
+        bool res=false;
+        for(int r=0;r<k;r++){
+            int last=r+((n-1-r)/k)*k;
             int s=0;
-            for(int j=i;j<n;j+=k){
-                s+=energy[j];
-            }
-            if(ok==0){ 
-                ans=s; 
-                ok=true;
-            }
-            else if(s>ans){
-                ans=s;
+            for(int i=last;i>=r;i-=k){
+                s+=energy[i];
+                if(!res){ 
+                    ans=s; 
+                    res=true; 
+                }
+                else if(s>ans){
+                    ans=s;
+                }
             }
         }
         return ans;
