1e389a8d9b47924ca09a0b29fb13bd1849d9d82a
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-10-16 10:28:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.cpp
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int findSmallestInteger(vector<int>& nums, int value) {
+        int v=value;
+        vector<int> mex(v);
         int n=nums.size();
-        vector<int>count(value,0);
-        //cnt remainders (both pos and neg)
         for(int i=0;i<n;i++){
-            int rem=nums[i]%value;
-            if(rem<0){
-                rem+=value;
-            }
-            count[rem]++;
+            int res=nums[i]%v;
+            if(res<0){
+                res+=v;
+            cnt[res]++;
         }
-        
-        //traversing and finding the maxm mex
-        int mex=0;
-        while(true){
-            int rem=mex%value;
-            if(count[rem]>0){
-                count[rem]--;
-                mex++;
-            }else{
-                break;
+        for(int x=0;;x++){
+            int res=x%v;
+            if(cnt[res]==0){
+                return x;
             }
+            cnt[res]--;
         }
-        
-        return mex;
     }
 };
