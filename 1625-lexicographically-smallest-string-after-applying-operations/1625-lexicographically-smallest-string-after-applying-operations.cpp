997b530672361177b43ce8dae868d569c79da132
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-10-19 13:35:21 +0530

    Time: 235 ms (53.1%), Space: 103.1 MB (30.53%) - LeetHub

--- 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.cpp
+++ 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.cpp
@@ -1,27 +1,34 @@
-class Solution {
+class Solution{
 public:
-    string findLexSmallestString(string s, int a, int b) {
+    string findLexSmallestString(string s,int a,int b){
         int n=s.size();
-        queue<string> q;
-        unordered_set<string>vis;
+        queue<string>q;
+        unordered_map<string,bool>vis; 
         string ans=s;
         q.push(s);
-        vis.insert(s);
+        vis[s]=true;
         while(!q.empty()){
             string cur=q.front();
             q.pop();
-            ans=min(ans,cur);
+            if(cur<ans){
+                ans=cur;
+            }
             string add=cur;
             for(int i=1;i<n;i+=2){
                 add[i]=((add[i]-'0'+a)%10)+'0';
             }
-            if(!vis.count(add)){
-                vis.insert(add);
+            if(!vis[add]){
+                vis[add]=true;
                 q.push(add);
             }
-            string rot=cur.substr(n-b)+cur.substr(0,n-b);
-            if(!vis.count(rot)){
-                vis.insert(rot);
+            string rot=cur;
+            string temp=cur;
+            for(int i=0;i<n;i++){
+                int idx=(i+b)%n;
+                rot[idx]=temp[i];
+            }
+            if(!vis[rot]){
+                vis[rot]=true;
                 q.push(rot);
             }
         }
