761cb76c0760004e9f4fb4dea68725eb7db2ab89
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-10-27 11:52:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,39 +1,22 @@
-//converting the linked lists to arrays and then performing sum index by index
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        vector<int> a,b;
-        while(l1!=0){ 
-            a.push_back(l1->val); 
-            l1=l1->next; 
-        }
-        while(l2!=0){
-            b.push_back(l2->val); 
-            l2=l2->next; 
-        }
-        int n=max(a.size(),b.size());
+        ListNode *dum=new ListNode(0);
+        ListNode *curr=dum;
         int carry=0;
-        ListNode *head=new ListNode(0);
-        ListNode *p=head;
-        for(int i=0;i<n||carry;i++){
-            int x,y;
-            if(i<a.size()){
-                x=a[i];
+        while(l1!=NULL || l2!=NULL || carry!=0){
+            int sum=carry;
+            if(l1!=NULL){
+                sum+=l1->val;
+                l1=l1->next;
+            }if(l2!=NULL){
+                sum+=l2->val;
+                l2=l2->next;
             }
-            else{
-                x=0;
-            }
-            if(i<b.size()){
-                y=b[i];
-            }
-            else{
-                y=0;
-            }
-            int s=x+y+carry;
-            p->next=new ListNode(s%10);
-            p=p->next;
-            carry=s/10;
+            carry=sum/10;
+            curr->next=new ListNode(sum%10);
+            curr=curr->next;
         }
-        return head->next;
+        return dummy->next;
     }
 };
