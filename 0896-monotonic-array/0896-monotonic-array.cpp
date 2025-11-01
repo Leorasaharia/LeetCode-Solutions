061cb736f8d3fc9356a16eaf103b51ad03e4a12d
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-11-01 18:42:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -1,17 +1,19 @@
-class Solution {
+class Solution{
 public:
-    bool isMonotonic(vector<int>& nums) {
+    bool isMonotonic(vector<int>&nums){
         int n=nums.size();
         bool inc=true;
         bool dec=true;
-        for (int i=1;i<n;i++){
-            if(nums[i]>nums[i-1]){
-                dec=false;
-            }if(nums[i]<nums[i-1]){
-                inc=false;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]>nums[j]){
+                    inc=false;
+                }
+                if(nums[i]<nums[j]){
+                    dec=false;
+                }
             }
         }
         return inc||dec;
     }
-
 };
