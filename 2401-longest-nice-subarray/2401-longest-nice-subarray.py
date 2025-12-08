9883db3c729955071fd64a348381f91211e8bf8f
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-12-08 13:44:30 +0530

    Time: 104 ms (39.47%), Space: 21.7 MB (81.58%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.py
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.py
@@ -3,12 +3,14 @@ class Solution(object):
         n=len(nums)
         if n==0:
             return None
-        sub=nums[0]
+        sub=1
+        used=0
+        left=0 
         for i in range(n):
-            curr=0
-            for j in range(i,n):
-                curr^=nums[j]
-                if curr>sub:
-                    sub=curr
+            while used &nums[i]!=0:
+                used^=nums[left]
+                left+=1
+            used|=nums[i]
+            sub=max(sub,i-left+1)
         return sub
-        
+
