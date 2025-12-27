9de61e2724ecc7d6d887f990f8714b96e107d83b
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-12-27 14:02:04 +0530

    Time: 0 ms (100%), Space: 10.3 MB (97.89%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,17 @@
-class Solution {
+class Solution{
 public:
-    bool stoneGame(vector<int>& piles) {
+    bool stoneGame(vector<int>& piles){
+        int n=piles.size();
+        int even=0;
+        int odd=0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                even+=piles[i];
+            }
+            else{
+                odd+=piles[i];
+            }
+        }
         return true;
     }
 };
