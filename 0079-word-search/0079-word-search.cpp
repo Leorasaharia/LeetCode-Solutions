a7a862acf76106c8ac0df101bce87fd8ff5ab7ef
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-12-28 12:54:37 +0530

    Time: 23 ms (98.26%), Space: 19.5 MB (8.16%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,26 +1,74 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& b, string& w, int i, int j, int k) {
-        if (k == w.size()){
-            return true;
-        }
-        if (i < 0 || j < 0 || i >= b.size() || j >= b[0].size() || b[i][j] != w[k]){
-            return false;
-        }
-        char tmp = b[i][j];
-        b[i][j] = '#';
-        bool found = dfs(b, w, i+1, j, k+1) || dfs(b, w, i-1, j, k+1) ||
-                     dfs(b, w, i, j+1, k+1) || dfs(b, w, i, j-1, k+1);
-        b[i][j] = tmp;
-        return found;
-    }
+    struct Node {
+        int x;
+        int y;
+        int k;
+        string vis;
+    };
     bool exist(vector<vector<char>>& b, string w) {
-        int m = b.size(), n = b[0].size();
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (b[i][j] == w[0] && dfs(b, w, i, j, 0)) {
-                    return true;
+        int m = b.size();
+        int n = b[0].size();
+        int cnt[128]={0};
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                cnt[b[i][j]]++;
+            }
+        }
+        for(int i=0;i<w.size();i++){
+            cnt[w[i]]--;
+            if(cnt[w[i]]<0)
+                return false;
+        }
+        if(cnt[w[0]] > cnt[w[w.size()-1]]){
+            int l=0,r=w.size()-1;
+            while(l<r){
+                char t=w[l];
+                w[l]=w[r];
+                w[r]=t;
+                l++; r--;
+            }
+        }
+        int d[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
+        queue<Node> q;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(b[i][j]==w[0]){
+                    string v(m*n,'0');
+                    v[i*n+j]='1';
+                    Node t;
+                    t.x=i;
+                    t.y=j;
+                    t.k=1;
+                    t.vis=v;
+                    q.push(t);
                 }
+            }
+        }
+        while(!q.empty()){
+            Node cur=q.front();
+            q.pop();
+            if(cur.k==w.size())
+                return true;
+            for(int i=0;i<4;i++){
+                int nx=cur.x+d[i][0];
+                int ny=cur.y+d[i][1];
+                if(nx<0 || ny<0 || nx>=m || ny>=n)
+                    continue;
+                int id=nx*n+ny;
+                if(cur.vis[id]=='1')
+                    continue;
+                if(b[nx][ny]!=w[cur.k])
+                    continue;
+                Node nxt;
+                nxt.x=nx;
+                nxt.y=ny;
+                nxt.k=cur.k+1;
+                nxt.vis=cur.vis;
+                nxt.vis[id]='1';
+                q.push(nxt);
+            }
+        }
         return false;
     }
 };
