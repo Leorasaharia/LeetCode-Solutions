5f463c9f22c5cbddb0581f7b8d1ebc9ed346322c
Leora <143778430+Leorasaharia@users.noreply.github.com>
2025-12-30 13:15:41 +0530

    Time: 84 ms (60.05%), Space: 44.8 MB (91.96%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int numMatchingSubseq(string s, vector<string>& words) {
-        int n=s.size();
-        int m=words.size();
+    int numMatchingSubseq(string s,vector<string>&words){
+        int n=words.size();
+        unordered_map<string,int>mp;
+        for(int i=0;i<n;i++){
+            mp[words[i]]++;
+        }
+        int m=s.size();
         int cnt=0;
-        for(int i=0;i<m;i++){
-            int j=0;
-            int k=0;
-            while(j<n && k<words[i].size()){
-                if(s[j]==words[i][k]){
+        for(auto it:mp){
+            string w=it.first;
+            int freq=it.second;
+            int j=0,k=0;
+            while(j<m  && k<w.size()){
+                if(s[j]==w[k]){
                     k++;
                 }
                 j++;
             }
-            if(k==words[i].size()){
-                cnt++;
+            if(k==w.size()){
+                cnt+=freq;
             }
         }
         return cnt;
