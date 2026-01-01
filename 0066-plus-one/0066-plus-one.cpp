74bc84ce2e83a3a73eb1ed3087be11d49a3c5e60
Leora <143778430+Leorasaharia@users.noreply.github.com>
2026-01-01 19:35:23 +0530

    Time: 0 ms (100%), Space: 11.3 MB (92.86%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,15 +1,27 @@
 class Solution{
 public:
     vector<int> plusOne(vector<int>&digits){
-        int n=digits.size();
-        for(int i=n-1;i>=0;i--){
-            if(digits[i]<9){
-                digits[i]++;
-                return digits;
-            }
-            digits[i]=0;
+        string s="";
+        for(int i=0;i<digits.size();i++){
+            s+=char(digits[i]+'0');
         }
-        digits.insert(digits.begin(),1);
-        return digits;
+
+        int i=s.length()-1;
+        while(i>=0&&s[i]=='9'){
+            s[i]='0';
+            i--;
+        }
+
+        if(i<0){
+            s='1'+s;
+        }else{
+            s[i]++;
+        }
+
+        vector<int> res;
+        for(int i=0;i<s.length();i++){
+            res.push_back(s[i]-'0');
+        }
+        return res;
     }
 };
